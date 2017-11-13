1b3c1cdb40768940d4c9c8b5061adab1567ba015
Mostafa Amin <Mostafa_chita@yahoo.com>
2017-11-13 00:26:23 -0500

    Issue#5

--- src/comp6231/project/mostafa/clientSide/AdminClient.java
+++
@@ -1,8 +0,0 @@
-package comp6231.project.mostafa.clientSide;
-
-public class AdminClient extends Client {
-	public AdminClient(String id){
-		this.setId(id);
-		initializeLog();
-	}
-}
--- src/comp6231/project/mostafa/clientSide/StudentClient.java
+++
@@ -1,8 +0,0 @@
-package comp6231.project.mostafa.clientSide;
-
-public class StudentClient extends Client {
-	public StudentClient(String id){
-		this.setId(id);
-		initializeLog();
-	}
-}
