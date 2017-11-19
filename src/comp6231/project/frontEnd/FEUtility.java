3869973c3eea21b948cb9c85635327eb328218b9
Mostafa Amin <Mostafa_chita@yahoo.com>
2017-11-19 01:31:37 -0500

    for test only

--- src/comp6231/project/frontEnd/FEUtility.java
+++ src/comp6231/project/frontEnd/FEUtility.java
@@ -1,5 +1,6 @@
 package comp6231.project.frontEnd;
 
+import comp6231.project.farid.sharedPackage.DrrsConstants;
 import comp6231.project.mostafa.core.Constants;
 
 public class FEUtility {
@@ -25,6 +26,18 @@ public class FEUtility {
 			return Constants.KKL_PORT_LISTEN;
 		}else if(id.contains("WST") || id.contains("wst")) {
 			return Constants.WST_PORT_LISTEN;
+		}else{
+			return -1;	
+		}
+	}
+	
+	public int findFaridUDPListenerPort(String id){
+		if(id.contains("DVL") || id.contains("dvl")){
+			return DrrsConstants.DVL_PORT;
+		}else if (id.contains("KKL") || id.contains("kkl")){
+			return DrrsConstants.KKL_PORT;
+		}else if(id.contains("WST") || id.contains("wst")) {
+			return DrrsConstants.WST_PORT;
 		}else{
 			return -1;	
 		}
