2259b5f9d3e91d2be6dcd5e3ac99186f787fdd52
Mostafa Amin <Mostafa_chita@yahoo.com>
2017-12-01 18:14:56 -0500

    port switchetre #2

--- src/comp6231/project/frontEnd/PortSwitcher.java
+++ src/comp6231/project/frontEnd/PortSwitcher.java
@@ -6,25 +6,25 @@ public class PortSwitcher {
 	
 	public static void switchServer(String campusCode) {
 		if (campusCode.equals("DVL")) {
-			changePort(Constants.dvlPortListenFaridActive, Constants.DVL_PORT_LISTEN_FARID_ORIGINAL, Constants.DVL_PORT_LISTEN_FARID_BACKUP);
-			changePort(Constants.dvlPortListenRe1Active, Constants.DVL_PORT_LISTEN_RE1_ORIGINAL, Constants.DVL_PORT_LISTEN_RE1_BACKUP);
-			changePort(Constants.dvlPortListenRe2Active, Constants.DVL_PORT_LISTEN_RE2_ORIGINAL, Constants.DVL_PORT_LISTEN_RE2_BACKUP);
+			Constants.dvlPortListenFaridActive = changePort(Constants.dvlPortListenFaridActive, Constants.DVL_PORT_LISTEN_FARID_ORIGINAL, Constants.DVL_PORT_LISTEN_FARID_BACKUP);
+			Constants.dvlPortListenRe1Active = changePort(Constants.dvlPortListenRe1Active, Constants.DVL_PORT_LISTEN_RE1_ORIGINAL, Constants.DVL_PORT_LISTEN_RE1_BACKUP);
+			Constants.dvlPortListenRe2Active = changePort(Constants.dvlPortListenRe2Active, Constants.DVL_PORT_LISTEN_RE2_ORIGINAL, Constants.DVL_PORT_LISTEN_RE2_BACKUP);
 		} else if (campusCode.equals("KKL")) {
-			changePort(Constants.kklPortListenFaridActive, Constants.KKL_PORT_LISTEN_FARID_ORIGINAL, Constants.KKL_PORT_LISTEN_FARID_BACKUP);
-			changePort(Constants.kklPortListenRe1Active, Constants.KKL_PORT_LISTEN_RE1_ORIGINAL, Constants.KKL_PORT_LISTEN_RE1_BACKUP);
-			changePort(Constants.kklPortListenRe2Active, Constants.KKL_PORT_LISTEN_RE2_ORIGINAL, Constants.KKL_PORT_LISTEN_RE2_BACKUP);
+			Constants.kklPortListenFaridActive = changePort(Constants.kklPortListenFaridActive, Constants.KKL_PORT_LISTEN_FARID_ORIGINAL, Constants.KKL_PORT_LISTEN_FARID_BACKUP);
+			Constants.kklPortListenRe1Active = changePort(Constants.kklPortListenRe1Active, Constants.KKL_PORT_LISTEN_RE1_ORIGINAL, Constants.KKL_PORT_LISTEN_RE1_BACKUP);
+			Constants.kklPortListenRe2Active = changePort(Constants.kklPortListenRe2Active, Constants.KKL_PORT_LISTEN_RE2_ORIGINAL, Constants.KKL_PORT_LISTEN_RE2_BACKUP);
 		} else if (campusCode.equals("WST")) {
-			changePort(Constants.wstPortListenFaridActive, Constants.WST_PORT_LISTEN_FARID_ORIGINAL, Constants.WST_PORT_LISTEN_FARID_BACKUP);
-			changePort(Constants.wstPortListenRe1Active, Constants.WST_PORT_LISTEN_RE1_ORIGINAL, Constants.WST_PORT_LISTEN_RE1_BACKUP);
-			changePort(Constants.wstPortListenRe2Active, Constants.WST_PORT_LISTEN_RE2_ORIGINAL, Constants.WST_PORT_LISTEN_RE2_BACKUP);
+			Constants.wstPortListenFaridActive = changePort(Constants.wstPortListenFaridActive, Constants.WST_PORT_LISTEN_FARID_ORIGINAL, Constants.WST_PORT_LISTEN_FARID_BACKUP);
+			Constants.wstPortListenRe1Active = changePort(Constants.wstPortListenRe1Active, Constants.WST_PORT_LISTEN_RE1_ORIGINAL, Constants.WST_PORT_LISTEN_RE1_BACKUP);
+			Constants.wstPortListenRe2Active = changePort(Constants.wstPortListenRe2Active, Constants.WST_PORT_LISTEN_RE2_ORIGINAL, Constants.WST_PORT_LISTEN_RE2_BACKUP);
 		}	
 	}
-	
-	private static void changePort(int active,int original, int backup) {
+
+	private static int changePort(int active,int original, int backup) {
 		if (active == original){
-			active = backup;
+			return backup;
 		} else {
-			active = original;
+			return original;
 		}
 	}
 }
